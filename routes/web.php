41e3d6a1d2d1071d18e3131ad9e1f8d3e919a1f1
030439 <praimchand763@gmail.com>
2025-12-24 16:52:36 +0500

    StudentController

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function () {
+
+    Route::resource('students', StudentController::class);
+
+    Route::post('pocket-money', [PocketMoneyController::class, 'store']);
+    Route::get('pocket-money/report', [PocketMoneyController::class, 'monthlyReport']);
+
+    Route::resource('laundry-staff', LaundryStaffController::class);
+    Route::post('laundry-records', [LaundryController::class, 'store']);
+    Route::get('laundry/report', [LaundryController::class, 'monthlyReport']);
+});
